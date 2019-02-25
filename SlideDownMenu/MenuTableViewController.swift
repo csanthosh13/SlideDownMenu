a1d1f58b48cd3e9ba03d17932cae6de95e3345b6
Santhosh Chandrasekharan <csanthosh13@gmail.com>
2019-02-25 14:09:45 +0530

    Initial Commit

---
+++ SlideDownMenu/MenuTableViewController.swift
@@ -0,0 +1,51 @@
+//
+//  MenuTableViewController.swift
+//  SlideDownMenu
+
+
+import UIKit
+
+class MenuTableViewController: UITableViewController {
+
+    var menuItems = ["Home", "News", "Tech", "Finance", "Reviews"]
+    var currentItem = "Home"
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // Return the number of sections
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // Return the number of rows
+        return menuItems.count
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! MenuTableViewCell
+
+        // Configure the cell...
+        cell.titleLabel.text = menuItems[indexPath.row]
+        cell.titleLabel.textColor = (menuItems[indexPath.row] == currentItem) ? UIColor.white : UIColor.gray
+
+        return cell
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let menuTableViewController = segue.source as! MenuTableViewController
+        if let selectedIndexPath = menuTableViewController.tableView.indexPathForSelectedRow {
+            currentItem = menuItems[selectedIndexPath.row]
+        }
+    }
+}
---
+++ SlideDownMenu/NewsTableViewCell.swift
@@ -0,0 +1,29 @@
+//
+//  NewsTableViewCell.swift
+//  SlideDownMenu
+
+
+import UIKit
+
+class NewsTableViewCell: UITableViewCell {
+
+    @IBOutlet var postImageView:UIImageView!
+    @IBOutlet var postTitle:UILabel!
+    @IBOutlet var postAuthor:UILabel!
+    @IBOutlet var authorImageView:UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        // Initialization code
+        authorImageView.layer.cornerRadius = authorImageView.frame.width / 2
+        authorImageView.layer.masksToBounds = true
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
